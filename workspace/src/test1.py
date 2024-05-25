1f54d34af44bb8fae2c973c275eda13e514be154
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2024-05-25 21:40:47 +0900

    test1

--- workspace/src/test1.py
+++ workspace/src/test1.py
@@ -2,9 +2,24 @@
 
 # create db session using sqlalchemy and postgresql+psycopg2 driver
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy import Metadata
+from sqlalchemy import Table, Column, Integer, String, Numeric, ForeignKey
+
+metadata = Metadata()
 
 # create engine
 engine = create_engine('postgresql+psycopg2://docker:docker@localhost:5432/docker')
 
 # create session
 connection = engine.connect()
+
+# create table
+cookies = Table('cookies', metadata,
+    Column('cookie_id', Integer(), primary_key=True),
+    Column('cookie_name', String(50), index=True),
+    Column('cookie_recipe_url', String(255)),
+    Column('cookie_sku', String(55)),
+    Column('quantity', Integer()),
+    Column('unit_cost', Numeric(12, 2))
+)
