953ea0b0de006939301a43ad823102fbd73cf177
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2024-05-26 10:31:34 +0900

    add working dirctory on windows machine

--- run.sh
+++ run.sh
@@ -8,7 +8,10 @@ CONTAINER_NAME="sqlalchemy_learning_container"
 
 # ホストのワークスペースディレクトリを設定
 ####!!!! 使用先のディレクトリを指定すること !!!!####
-HOST_WORKSPACE="/Users/mn/workspace/db_study/sql_alchemy_study/workspace"
+
+#HOST_WORKSPACE="/Users/mn/workspace/db_study/sql_alchemy_study/workspace"
+
+HOST_WORKSPACE="/home/mn/workspace/sql_alchemy_study/workspace"
 
 
 # コンテナ内のワークスペースディレクトリを設定
